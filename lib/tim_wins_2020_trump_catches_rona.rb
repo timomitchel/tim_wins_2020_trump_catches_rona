9441fb5a8e39b7c21ef5122bc7055c4108f8e11e
Tim Tyrrell <timomitchel23@gmail.com>
2020-07-10 14:37:16 -0600

    Update code and executable

--- lib/tim_wins_2020_trump_catches_rona.rb
+++ lib/tim_wins_2020_trump_catches_rona.rb
@@ -2,5 +2,9 @@ require "tim_wins_2020_trump_catches_rona/version"
 
 module TimWins2020TrumpCatchesRona
   class Error < StandardError; end
-  # Your code goes here...
+  class Vote
+    def for_tim
+      puts "Vote for Tim! Or, Anyone else but Trump. (Kanye works)"
+    end
+  end
 end
